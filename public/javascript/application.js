36a12bad35bfae77966418eeab5e9af9e2482d27
David Shorthouse <davidpshorthouse@gmail.com>
2014-11-21 23:10:06 -0500

    Swap shp files, remove unused files

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,10 +1,18 @@
 $(function() {
-  var map = L.map('map').setView([55, -70], 4);
+  var map = L.map('map').setView([55, -70], 4),
+  graticule = L.graticule({ interval: 10 });
+
   L.tileLayer('http://localhost:8888/{z}/{x}/{y}.png', {
       minZoom: 1,
       maxZoom: 9
   }).addTo(map);
-  map.on('load', function(e) {
-      console.log("loaded");
-  });  
+
+  $('#graticules').on("click", function() {
+    if ($(this).prop("checked")) {
+      map.addLayer(graticule);
+    } else {
+      map.removeLayer(graticule);
+    }
+  });
+
 });
